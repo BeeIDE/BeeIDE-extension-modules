f66835d2e4c3009bb5d1348ac9800b83cd7e2107
Le Quang Dung <lqdung95ntp@gmail.com>
2024-03-29 13:51:40 +0700

    Add BeeUltrasonic module

---
+++ main.js
@@ -0,0 +1,16 @@
+{
+    "BeeUltrasonic": {
+      "name": "BeeUltrasonic",
+      "description": "Ultrasonic sensor",
+      "author": "BeeBlock",
+      "category": "Sensor",
+      "version": "1.0.0",
+      "icon": "/static/ultrasonic.png",
+      "color": "#3498DB",
+      "chip": [
+        "ESP32"
+      ],
+      "depends": [],
+      "github": "https://github.com/BeeIDE/BeeUltrasonic"
+    }
+}
